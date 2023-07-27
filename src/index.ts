2b883ee84755063d79282f3add9dee32197c4acc
TechnoCoder08 <jansky.stepan08@gmail.com>
2023-07-28 00:29:34 +0200

    Uploaded all the files

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import { SmartLed, LED_WS2812 } from "smartled";
+import * as colors from "./libs/colors.js"
+import * as gpio from "gpio";
+import { stdout } from "stdio";
+import * as adc from "adc";
+import * as readline from "./libs/readline.js"
+import * as servo from "./libs/servo.js"
+
+// Your code here //
